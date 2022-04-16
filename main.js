ae1c481e0e45adf9567fb2080e67c37a282bce69
Mariano Kuran <marianokuran6@gmail.com>
2022-04-16 17:33:17 -0300

    Updated files

--- main.js
+++ main.js
@@ -3,19 +3,48 @@ const form = document.getElementById('form')
 form.addEventListener('submit', function(event){
     event.preventDefault();
     let datosForm = new FormData(form);
-    crearFila(datosForm)
+    let trsObj = datosFormAtrsObj(datosForm)
+    guardarTrsObj(trsObj)
+    crearFila(trsObj)
 });
 
-function crearFila(datosForm) {
+document.addEventListener('DOMContentLoaded', function (event){
+    let trsObjArr = JSON.parse(localStorage.getItem('trsDatos'))
+    trsObjArr.forEach(arrElement => {
+        crearFila(arrElement)
+    });
+})
+
+function datosFormAtrsObj(datosForm){
+    let tipo_trs = datosForm.get('tipo_trs')
+    let descripcion = datosForm.get('descripcion')
+    let monto = datosForm.get('monto')
+    let categoria = datosForm.get('categoria')
+    return {
+        'tipo_trs': tipo_trs,
+        'descripcion': descripcion,
+        'monto': monto,
+        'categoria': categoria
+    }
+}
+
+function crearFila(trsObj) {
     let tabla = document.getElementById('tabla');
     let nuevaFila = tabla.insertRow(-1);
 
     nuevaCelda = nuevaFila.insertCell(0);
-    nuevaCelda.textContent= datosForm.get('tipo_trs')
+    nuevaCelda.textContent= trsObj['tipo_trs'];
     nuevaCelda = nuevaFila.insertCell(1);
-    nuevaCelda.textContent= datosForm.get('descripcion')
+    nuevaCelda.textContent= trsObj['descripcion'];
     nuevaCelda = nuevaFila.insertCell(2);
-    nuevaCelda.textContent= datosForm.get('monto')
+    nuevaCelda.textContent= trsObj['monto'];
     nuevaCelda = nuevaFila.insertCell(3);
-    nuevaCelda.textContent= datosForm.get('categoria')
+    nuevaCelda.textContent= trsObj['categoria'];
+}
+
+function guardarTrsObj(trsObj){
+    let trsArr = JSON.parse(localStorage.getItem('trsDatos')) || [];
+    trsArr.push(trsObj)
+    let trsArrJSON = JSON.stringify(trsArr)
+    localStorage.setItem('trsDatos', trsArrJSON);
 }
